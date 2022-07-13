81989e32b036562807a73f337549f70023d0b8c5
groldi <chniucg@gmail.com>
2022-07-13 17:02:11 +0200

    Updated License
    Signed-off-by: groldi <chniucg@gmail.com>

--- src/main/java/de/kissenpvp/spigot/api/base/PluginSpigot.java
+++ src/main/java/de/kissenpvp/spigot/api/base/PluginSpigot.java
@@ -3,18 +3,18 @@
  * Copyright (C) KissenPvP team and contributors.
  *
  * This program is free software and is free to redistribute
- * and/or modify under the terms of the GNU General Public
- * License as published by the Free Software Foundation,
- * either version 3 of the License, or (at your option)
+ * and/or modify under the terms of the Apache
+ * License as published by the Apache Software Foundation,
+ * either version 2 of the License, or (at your option)
  * any later version.
  *
  * This program is intended for the purpose of joy,
  * WITHOUT WARRANTY without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- * See the GNU Lesser General Public License for more details.
+ * See the Apache License for more details.
  *
- * You should have received a copy of the GNU General Public License
- * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ * You should have received a copy of the Apache License
+ * along with this program. If not, see <https://www.apache.org/licenses/LICENSE-2.0>.
  */
 
 package de.kissenpvp.spigot.api.base;
